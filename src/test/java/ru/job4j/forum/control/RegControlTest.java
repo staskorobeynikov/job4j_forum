279d90db3ef7e2c40c8a6a96c581d6340d6c8b03
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-07-22 20:31:33 +0300

    add test for RegControl

--- src/test/java/ru/job4j/forum/control/RegControlTest.java
+++ src/test/java/ru/job4j/forum/control/RegControlTest.java
@@ -6,19 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import ru.job4j.forum.Main;
-import ru.job4j.forum.model.Topic;
+import ru.job4j.forum.model.Authority;
 import ru.job4j.forum.model.User;
+import ru.job4j.forum.repository.AuthorityRepository;
 import ru.job4j.forum.repository.UserRepository;
-import ru.job4j.forum.service.ForumService;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -32,6 +36,12 @@ class RegControlTest {
 
     @MockBean
     private UserRepository repository;
+
+    @MockBean
+    private AuthorityRepository authorities;
+
+    @MockBean
+    private PasswordEncoder encoder;
 
     @Test
     @WithMockUser
@@ -56,5 +66,32 @@ class RegControlTest {
         verify(repository).save(argument.capture());
         User value = argument.getValue();
         assertThat(value.getUsername(), is("root"));
+    }
+
+    @Test
+    public void whenRedirectRegJspWithAlreadyUsername() throws Exception {
+        this.mockMvc.perform(get("/reg?error=true"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("reg"))
+                .andExpect(model().attribute(
+                        "errorMessage",
+                        "User with this username is already registered!!")
+                );
+    }
+
+    @Test
+    public void whenRedirectRegJspWithErrorTrue() throws Exception {
+        Authority auth = new Authority();
+        auth.setAuthority("ROLE_USER");
+
+        when(encoder.encode(any())).thenReturn("");
+        when(authorities.findByAuthority(any())).thenReturn(auth);
+        when(repository.save(any())).thenThrow(DataIntegrityViolationException.class);
+
+        this.mockMvc.perform(post("/reg"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/reg?error=true"));
     }
 }
